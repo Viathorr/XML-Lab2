825c847bde2a560a2b415a215f663a4cdfaec12a
Viathorr <142295754+Viathorr@users.noreply.github.com>
2024-10-23 12:45:55 +0300

    Added logger to Candies class

--- src/main/java/com/xmlparsers/candies/Candies.java
+++ src/main/java/com/xmlparsers/candies/Candies.java
@@ -1,10 +1,15 @@
-package com.xmlparser.candies;
+package com.xmlparsers.candies;
+
+import com.xmlparsers.SAXCandiesParser;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 public class Candies {
+
+    private static final Logger logger = Logger.getLogger(SAXCandiesParser.class.getName());
 
     protected List<Candy> candies;
 
@@ -21,6 +26,7 @@ public class Candies {
     }
 
     public void sortCandies(Comparator<Candy> comparator) {
+        logger.info("Sorting candies in ascending order by Energy value.");
         this.candies.sort(comparator);
     }
 
